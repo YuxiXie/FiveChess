c35076dd1fa42ed48cfa910dcb0a18364e6c91a1
Jerry Xie <v-yuxie@microsoft.com>
2020-06-05 16:16:23 +0800

    adjust

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -7,15 +7,15 @@ import com.jtattoo.plaf.mint.MintLookAndFeel;
 public class Main {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-		@Override
-		public void run() {
-			try {
-				UIManager.setLookAndFeel(new MintLookAndFeel());
-			} catch (Exception e) {
-				e.printStackTrace();
+			@Override
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(new MintLookAndFeel());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				new Home();
 			}
-			new Home();
-		}
 		});
 	}
 }
